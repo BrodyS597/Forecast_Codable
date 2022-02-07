291548235de98a2f9706b6f66d9ae83cf1890a0e
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-02-07 13:11:15 -0700

    Completed the model conforming to codable protocol

---
+++ Forecast_Codable/Forecast_Codable/Controller/NetworkController.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkController.swift
+//  Forecast_Codable
+//
+//  Created by Brody Sears on 2/7/22.
+//
+
+import Foundation
+// api key: "ddae98f4e48d427f9c59a2ec2c5521c6"
+//baseURLString = "https://api.weatherbit.io/v2.0/forecast/daily"
+
+
